27f7eb9ea8fef9696e28894a601a28b716a9305f
Andrei Potapov <andr3008@gmail.com>
2021-10-14 23:24:57 +0300

    add array colors,refs to class

--- index.js
+++ index.js
@@ -1,48 +1,52 @@
-const colors = [
-	"#FFFFFF",
-	"#2196F3",
-	"#4CAF50",
-	"#FF9800",
-	"#009688",
-	"#795548",
-];
-const startRef = document.querySelector("[data-action='start']");
-const stopRef = document.querySelector("[data-action='stop']");
-
 class Timer {
-	constructor() {
+	constructor(colors, startRef, stopRef) {
 		this.intervalId = null;
 		this.isActive = false;
+		this.colors = colors = [
+			"#FFFFFF",
+			"#2196F3",
+			"#4CAF50",
+			"#FF9800",
+			"#009688",
+			"#795548",
+		];
+		this.startRef = startRef = document
+			.querySelector("[data-action='start']")
+			.addEventListener("click", () => {
+				timer.start();
+			});
+		this.stopRef = stopRef = document
+			.querySelector("[data-action='stop']")
+			.addEventListener("click", () => {
+				timer.stop();
+			});
 	}
 	start() {
 		if (this.isActive) return;
+
 		const startTime = Date.now();
 		this.isActive = true;
 		this.intervalId = setInterval(() => {
 			const currentTime = Date.now();
+			const deltaTime = currentTime - startTime;
 
-			console.log(currentTime - startTime);
-			changeColor();
+			this.changeColor(this.colors);
+			this.randomIntegerFromInterval();
+
+			console.log(deltaTime);
 		}, 1000);
 	}
 	stop() {
 		clearInterval(this.intervalId);
 		this.isActive = false;
 	}
+	changeColor(colors) {
+		const color = colors[this.randomIntegerFromInterval(0, colors.length)];
+		document.body.style.backgroundColor = color;
+	}
+	randomIntegerFromInterval(min, max) {
+		return Math.floor(Math.random() * (max - min + 1) + min);
+	}
 }
+
 const timer = new Timer();
-
-startRef.addEventListener("click", () => {
-	timer.start();
-});
-stopRef.addEventListener("click", () => {
-	timer.stop();
-});
-
-const randomIntegerFromInterval = (min, max) => {
-	return Math.floor(Math.random() * (max - min + 1) + min);
-};
-function changeColor() {
-	const color = colors[randomIntegerFromInterval(0, colors.length)];
-	document.body.style.backgroundColor = color;
-}
